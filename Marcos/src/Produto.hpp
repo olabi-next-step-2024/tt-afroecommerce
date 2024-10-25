61e9b21e13ece1ed1998114862e59a2d9d60f0e7
Marcos-PFreitas <marcospfreitas@google.com>
2024-10-25 14:43:41 -0300

    feature/cadastros-produtos

---
+++ Marcos/src/Produto.hpp
@@ -0,0 +1,45 @@
+// Produto.hpp
+#ifndef PRODUTO_HPP       // Se PRODUTO_HPP não foi definido ainda
+#define PRODUTO_HPP       // Define PRODUTO_HPP para evitar inclusões futuras
+
+#include <string>
+
+class Produto {
+protected:
+    std::string nomeProduto, descricaoProduto;
+    double precoProduto;
+    int quantidadeEstoque;
+    enum tipoProduto
+    { 
+        Fisico,
+        Digital
+    };
+    enum categoria{
+        Categoria1,
+        Categoria2,
+        Categoria3,
+        Categoria4
+    };
+
+public:
+     Produto(std::string nome, std::string descricao, double valor, int quantidade) 
+     : nomeProduto(nome), descricaoProduto(descricao), precoProduto(valor), quantidadeEstoque(quantidade) {}
+    
+    virtual ~Produto() {}
+
+    virtual void print() = 0;
+
+    // Getters
+    virtual std::string getNomeProduto() const = 0;
+    virtual std::string getDescricao() const = 0;
+    virtual double getPrecoProduto() const = 0;
+    virtual int getQuantidadeEstoque() const = 0;
+
+    // Setters
+    virtual void setNomeProduto(const std::string& nome) = 0;
+    virtual void setDescricao(const std::string& descricao) = 0;
+    virtual void setPrecoProduto(double preco) = 0;
+    virtual void setQuantidadeEstoque(int quantidade) = 0;
+};
+
+#endif // PRODUTO_HPP
